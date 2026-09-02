e04803fe796a92080f59363d3c1423114bc45c0a
parag giradkar <paraggiradkar9327@gmail.com>
2026-09-02 12:43:30 +0530

    added the sitemap file in the project

---
+++ app/sitemap.ts
@@ -0,0 +1,29 @@
+import { MetadataRoute } from "next";
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.unicorntechnologiess.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.unicorntechnologiess.com/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.unicorntechnologiess.com/services",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.unicorntechnologiess.com/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
